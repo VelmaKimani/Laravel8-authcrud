d236f35bc008f33205bc4c78817941d290ca7d33
savirafatika <savira.fatika@students.amikom.ac.id>
2020-11-27 06:35:18 +0700

    auth, crud, search, pagination, migration using jetstream livewire tailwind css

---
+++ app/Http/Livewire/Posts.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+use Livewire\WithPagination;
+
+class Posts extends Component
+{
+
+    use WithPagination;
+
+    // public $posts;
+    public $search;
+    public $postId, $title, $description;
+    public $isOpen = 0;
+
+    public function render()
+    {
+        // $this->posts = Post::all();
+        $searchParams = '%'.$this->search.'%';
+        return view('livewire.posts', [
+            'posts' => Post::where('title','like', $searchParams)->latest()->paginate(5)
+        ]);
+    }
+
+    public function showModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function hideModal()
+    {
+        $this->isOpen = false;
+    }
+
+    public function store()
+    {
+        $this->validate(
+            [
+                'title' => 'required',
+                'description' => 'required'
+            ]
+        );
+
+        Post::updateOrCreate(['id' => $this->postId], [
+            'title' => $this->title,
+            'description' => $this->description
+        ]);
+
+        $this->hideModal();
+
+        session()->flash('info', $this->postId ? 'Post update Successfully' : 'Post Created Successfully');
+
+        $this->postId = '';
+        $this->title = '';
+        $this->description = '';
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $this->postId = $id;
+        $this->title = $post->title;
+        $this->description = $post->description;
+
+        $this->showModal();
+    }
+
+    public function delete($id)
+    {
+        Post::find($id)->delete();
+        session()->flash('delete', 'Post Successfully Deleted');
+    }
+}
